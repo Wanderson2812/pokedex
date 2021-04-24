7a1b72b38b2054e1de730a56055e684556c35484
Wanderson <wandersonjr28@gmail.com>
2021-04-24 12:49:47 -0300

    botao de pesquisar pokemon, ajuste do responsivel

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,16 @@
-var objUrl;
-function getListPokemon(url) {
+function getUrl(url) {
     $.ajax({
-        url: url || "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1",
+        url: url || "https://pokeapi.co/api/v2/pokemon/3",
         success: function (req) {
-            objUrl = req;
-            getUrlPokemon(objUrl);
-            //getNextPrevUrl(req);
+            createObject(req);
+            nextPrevious(req);
+            search();
         }
     });
 }
-getListPokemon();
+getUrl();
 
-function getUrlPokemon(req) {
-    var listUrlPokemon = req.results;
-
-    for (let prop in listUrlPokemon) {
-        debugger
-        var url = listUrlPokemon[prop].url;
-        $.ajax({
-            url: url,
-            async: false,
-            success: function (req) {
-                createObjectPokemon(req);
-                nextPreviousPokemon();
-            }
-        });
-    }
-
-    // if (!$('.content').hasClass('slick-slider')) {
-    //     debugger
-    //     $('.content').slick({
-    //         useCSS: false,
-    //         arrows: false,
-    //         mobileFirst: true,
-    //         infinite: false
-    //     });
-    // } else {
-    //     $('.content').slick('refresh');
-    //}
-}
-
-function createObjectPokemon(obj) {
-    debugger
+function createObject(obj) {
     var objPokemon = {
         name: obj.name,
         image: obj.sprites.other["official-artwork"].front_default,
@@ -49,28 +18,26 @@ function createObjectPokemon(obj) {
         stats: obj.stats,
         id: obj.id
     }
-    appendCardPokemon(objPokemon);
+
+    appendCard(objPokemon);
+    getVariantForm(obj);
 }
 
-function appendCardPokemon(objPokemon) {
+function appendCard(objPokemon) {
     $('.card-body.old-card').remove();
     objPokemon.name = objPokemon.name.charAt(0).toUpperCase() + objPokemon.name.slice(1);
 
-    // if (lastIndex) {
-    //     $('section .content').slick('slickAdd', appendPokemon);
-    // } else {
     $('section .content').append(appendPokemon);
-    //}
 
-    var $this = $('.card-body').last().attr('id', objPokemon.id);
-    getInfoPokemon(objPokemon, $this);
+    var $this = $('.card-body').attr('id', objPokemon.id);
+    getInfo(objPokemon, $this);
 
     $this.find('figure.pokemon-image img').attr('src', objPokemon.image).attr('alt', objPokemon.name);
     $this.find('.pokemon-info-top .pokemon-name-number h1').text(objPokemon.name);
     $this.find('.pokemon-info-top .pokemon-name-number h2').text("#" + objPokemon.id);
 }
 
-function getInfoPokemon(objPokemon, $this) {
+function getInfo(objPokemon, $this) {
     var colorType = [];
 
     //get color type pokemon
@@ -80,10 +47,10 @@ function getInfoPokemon(objPokemon, $this) {
 
         colorType.push(type.name);
 
-        $this.find(".pokemon-types .pokemon-type").append('<li class="type' + slot + '"></li>');
+        $this.find(".pokemon-type-form .pokemon-type").append('<li class="type' + slot + '"></li>');
         $this.find(".type" + slot + "").text(type.name.toUpperCase());
         $this.find(".type" + slot + "").css("background", "var(--" + type.name + ")");
-        $this.find(".type" + slot + "").css("border", "var(--bs-" + type.name + ")");
+        $this.find(".type" + slot + "").css("border", "var(--b-" + type.name + ")");
 
         if (objPokemon.types.length == 1) {
             $this.find(".type" + slot + "").addClass('monoType');
@@ -94,7 +61,6 @@ function getInfoPokemon(objPokemon, $this) {
     for (var j = 0; j < objPokemon.stats.length; j++) {
         var type_stat = objPokemon.stats[j].stat.name;
         var base_type_stat = objPokemon.stats[j].base_stat;
-        var nameStat;
 
         $this.find(".pokemon-stats-base").append('<ul class="pokemon-stats">\n\
                                                         <li class="stats-name ' + type_stat + '"></li>\n\
@@ -105,23 +71,29 @@ function getInfoPokemon(objPokemon, $this) {
                                                     </ul>');
 
         var $nameStat = $this.find("." + type_stat + "");
-        if (type_stat == 'hp') {
-            $nameStat.text(type_stat.toUpperCase());
-        } else if (type_stat == 'attack') {
-            nameStat = 'atk';
-            $nameStat.text(nameStat.toUpperCase());
-        } else if (type_stat == 'defense') {
-            nameStat = 'def';
-            $nameStat.text(nameStat.toUpperCase());
-        } else if (type_stat == 'special-attack') {
-            nameStat = 'sp.atk';
-            $nameStat.text(nameStat.toUpperCase());
-        } else if (type_stat == 'special-defense') {
-            nameStat = 'sp.def';
-            $nameStat.text(nameStat.toUpperCase());
-        } else if (type_stat == 'speed') {
-            $nameStat.text(type_stat.toUpperCase());
+        var nameStat;
+        switch (type_stat) {
+            case 'hp':
+                nameStat = 'hp';
+                break;
+            case 'attack':
+                nameStat = 'atk';
+                break;
+            case 'defense':
+                nameStat = 'def';
+                break;
+            case 'special-attack':
+                nameStat = 'sp.atk';
+                break;
+            case 'special-defense':
+                nameStat = 'sp.def';
+                break;
+            case 'speed':
+                nameStat = 'speed';
+                break;
         }
+
+        $nameStat.text(nameStat.toUpperCase());
 
         var $statValue = $this.find("." + base_type_stat + "");
         $statValue.text(base_type_stat);
@@ -130,99 +102,78 @@ function getInfoPokemon(objPokemon, $this) {
 
     //validation apply color background by type pokemon
     if (colorType.length > 1) {
-        debugger
-        var colorTypeDuo = colorType.toString().replace(',', '-');
-        $this.parents('body').css("background", "var(--bg-" + colorTypeDuo + ")");
+        $this.parents('body').css("background", "linear-gradient(90deg, var(--bg-" + colorType[0] + ") 50%, var(--bg-" + colorType[1] + ") 50%)");
     } else {
-        var colorTypeMono = colorType.toString().replace(',', '-');
-        $this.parents('body').css("background", "var(--bg-" + colorTypeMono + ")");
+        $this.parents('body').css("background", "var(--bg-" + colorType[0] + ")");
     }
 }
 
-function nextPreviousPokemon() {
-    debugger
-    var next = objUrl.next;
-    var previous = objUrl.previous;
-
+function nextPrevious(req) {
+    var getNextPokemon = req;
     $(".next").unbind('click');
-    $('.next').on('click', function () {
-        debugger
-        $('.content').find('.card-body').addClass('old-card');
-        getListPokemon(next);
-        // if (slickNextPrev.currentSlideB < slickNextPrev.nextSlideB) {
-        //     countIndex = countIndex + 1;
-        //     slickNextPrev.direction = 'right';
-        // }
-    });
-
     $(".previous").unbind('click');
-    $('.previous').on('click', function () {
-        debugger
-        $('.content').find('.card-body').addClass('old-card');
-        getListPokemon(previous);
-        // if (slickNextPrev.currentSlideB > slickNextPrev.nextSlideB) {
-        //     countIndex = countIndex - 1;
-        //     slickNextPrev.direction = 'left';
-        // }
+
+    $('.content').find('.card-body').addClass('old-card');
+
+    $('.next').on('click', function () {
+        var id = getNextPokemon.id + 1;
+        var urlNext = "https://pokeapi.co/api/v2/pokemon/" + id + "";
+        getUrl(urlNext);
     });
 
+    $('.previous').on('click', function () {
+        var id = getNextPokemon.id - 1;
+        var urlPrevious = "https://pokeapi.co/api/v2/pokemon/" + id + "";
+        getUrl(urlPrevious);
+    });
 }
 
-// function getNextPrevUrl(req) {
-//     //debugger
-//     urlNext = req.next;
-//     urlPrev = req.previous;
-// }
+function search() {
+    $('.pokemon-search-image img').unbind('click');
+    $('.pokemon-search-image img').on('click', function () {
+        var $inputSearch = $(this).parent('figure').find('input');
 
-// var lastIndex, countIndex = 1;
-// function slickSlide(req) {
-//     $('.content').slick({
-//         useCSS: false,
-//         mobileFirst: true,
-//         infinite: false,
-//         // nextArrow: '<span class="slick-next"></span>',
-//         // prevArrow: '<span class="slick-prev"></span>',
-//         edgeFriction: 0.5,
-//         speed: 800
-//     });
+        if ($('.pokemon-search-input').is(':visible')) {
+            $(this).attr('src', 'assets/img/poke-lock.png');
+            $inputSearch.hide();
+        } else {
+            $(this).attr('src', 'assets/img/poke-open.png');
+            $inputSearch.show();
 
-//     $('.content').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-//         debugger
-//         slick.nextSlideB = nextSlide;
-//         slick.currentSlideB = currentSlide;
-//     });
+        }
 
-//     $('.content').on('afterChange', function (slick, currentSlide) {
-//         debugger
-//         nextPreviousPokemon();
-//         //$('.content').slick('refresh');
+        $inputSearch.unbind('change');
+        $inputSearch.change(function () {
+            var id = $(this).val();
+            var urlSearch = "https://pokeapi.co/api/v2/pokemon/" + id + "";
+            getUrl(urlSearch);
 
-//         // if (countIndex > currentSlide.slideCount - 1) {
-//         //     lastIndex = true;
-//         //     getListPokemon(urlNext);
-//         // }
-//     });
+            $('.pokemon-search-image img').attr('src', 'assets/img/poke-lock.png');
+            $inputSearch.val('').hide();
+        });
+    });
+}
 
-//     $('.content').on('reInit', function (slick) {
-//         debugger
-//         nextPreviousPokemon();
-//         // var slickReinit = $('.content').slick('getSlick');
-//         // var totalSlide = slickReinit.slideCount;
-//         // var totalSlideAppend = $('.content').find('.card-body').length;
-//         // var removeOldSlides = totalSlideAppend / 2;
-//     });
+function getVariantForm(obj) {
+    var urlSpecie = obj.species.url;
+    $.ajax({
+        url: urlSpecie,
+        success: function (req) {
+            var variant = req.varieties;
+            appendVariantForm(variant);
+        }
+    });
+}
 
-//     $('.content').on('init', function (slick) {
-//         debugger
-//         nextPreviousPokemon();
-//     });
-
-//     // $('.content').on('edge', function (event, slick, direction) {
-//     //     //debugger
-//     //     slick.lastIndex = true;
-//     //     slick.directionEdge = direction;
-//     // });
-// }
+function appendVariantForm(variant) {
+    var variantForm = variant;
+    for (let prop in variantForm) {
+        if (!variantForm[prop].is_default) {
+            debugger
+            var variantType = variantForm[prop].pokemon.name.replace(/(^[a-z]+\-+)/g, '');
+        }
+    }
+}
 
 var appendPokemon =
     '<div class="card-body">\n\
@@ -234,7 +185,7 @@ var appendPokemon =
                 <h1 class="pokemon-name"></h1>\n\
                 <h2 class="pokemon-number"></h2>\n\
             </div>\n\
-            <div class="pokemon-types">\n\
+            <div class="pokemon-type-form">\n\
                 <ul class="pokemon-type"></ul>\n\
             </div>\n\
         </div>\n\
@@ -242,3 +193,17 @@ var appendPokemon =
             <h1>Base Stats</h1>\n\
         </div>\n\
     </div>';
+
+var appendButtomPokemonVariantForm =
+'';
+
+
+
+/* <div class="pokemon-variants">\n\
+                    <div class="mega">\n\
+                        <img src="assets/img/mega-evolution.png" alt="MegaEvolution">\n\
+                    </div>\n\
+                    <div class="gmax">\n\
+                        <img src="assets/img/gigantamax.png" alt="GigantaMax">\n\
+                    </div>\n\
+                </div>\n\ */
